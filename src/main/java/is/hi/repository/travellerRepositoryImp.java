d2fd39ef20e4575446d83f8d5f2f038fc548c2bc
ofm1 <31484189+ofm1@users.noreply.github.com>
2017-09-17 17:21:00 +0000

    Add files via upload

---
+++ src/main/java/is/hi/repository/travellerRepositoryImp.java
@@ -0,0 +1,28 @@
+package is.hi.repository;
+
+
+import is.hi.model.Traveller;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class travellerRepositoryImp implements travellerRepository {
+    private final List<Traveller> travellers;
+   // private Traveller mockCamp;
+    public travellerRepositoryImp(){
+        this.travellers = new ArrayList<Traveller>();
+    }
+
+    @Override
+    public List<Traveller> getAll(){
+        Traveller traveller = new Traveller("ofm1", "ofm1@hi.is", "password");
+
+        return travellers;
+    }
+    @Override
+    public void add (Traveller traveller){
+        travellers.add(traveller);
+    }
+}
