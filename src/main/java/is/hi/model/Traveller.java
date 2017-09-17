d4529a93cba3023aba1b17250e158e4ce6ceba40
ofm1 <31484189+ofm1@users.noreply.github.com>
2017-09-17 17:47:38 +0000

    Update Traveller.java

--- src/main/java/is/hi/model/Traveller.java
+++ src/main/java/is/hi/model/Traveller.java
@@ -4,6 +4,12 @@ public class Traveller {
     String username;
     String email;
     String password;
+    
+    public Traveller(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
