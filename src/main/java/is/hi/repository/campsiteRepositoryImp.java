2d152d58d23fc06f755168a5cb8d7ea692b57fcc
ofm1 <31484189+ofm1@users.noreply.github.com>
2017-09-18 17:31:44 +0000

    Update campsiteRepositoryImp.java

--- src/main/java/is/hi/repository/campsiteRepositoryImp.java
+++ src/main/java/is/hi/repository/campsiteRepositoryImp.java
@@ -11,17 +11,17 @@ public class campsiteRepositoryImp implements campsiteRepository {
     private final List<Camp> camps;
    // private Camp mockCamp;
     public campsiteRepositoryImp(){
-        this.camps = new ArrayList<Camp>();
-    }
-
-    @Override
-    public List<Camp> getAll(){
         Camp camp = new Camp("Tjaldsvæði",1000,"no service","05:00-22:00","no shops", "no entertainment","South",8,2);
         Camp camp1 = new Camp("Tjaldsvæði1",1000,"no service","05:00-22:00","no shops", "no entertainment","South",8,2);
         camps.add(camp1);
         Camp camp2 = new Camp("Tjaldsvæði2",2000,"so much service","05:00-22:00","very good shops", "PARTY","North",10,3);
         camps.add(camp2);
         camps.add(camp);
+        this.camps = new ArrayList<Camp>();
+    }
+
+    @Override
+    public List<Camp> getAll(){
         return camps;
     }
     @Override
