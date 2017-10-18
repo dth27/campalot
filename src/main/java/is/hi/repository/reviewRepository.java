c7f74b4b3c3d35ee35e6c8784e3e576731349c63
oloff <ofm1@hi.is>
2017-10-18 21:10:19 +0000

    fixed

--- src/main/java/is/hi/repository/reviewRepository.java
+++ src/main/java/is/hi/repository/reviewRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 @Repository
@@ -19,7 +20,8 @@ public interface reviewRepository extends JpaRepository<Review, Long> {
         @Query(value = "SELECT n FROM Review n")
         ArrayList<Review> getAll();
 
-
+    @Transactional
+    @Modifying
     @Query(value = "insert into review(review,campname,username) VALUES (?1, ?2, ?3)"
             , nativeQuery = true)
     void addReview(String myReview, String name, String user);
