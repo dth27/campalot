0b08b47d861b1e1743af1ad095f087265397958a
dth27 <dth27@hi.is>
2017-10-26 11:20:09 +0000

    run program test

---
+++ src/test/java/is/hi/RunProgram.java
@@ -0,0 +1,29 @@
+package is.hi;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * @author Dilja
+ * (largely based on code from:)
+ * @author Ebba Þóra Hvannberg
+ *
+ * @date 26. October 2017
+ * HBV501G Hugbúnaðarverkefni 1 Háskóli Íslands
+ *
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunProgram {
+
+    @Test
+    public void contextLoads() {
+    }
+}
