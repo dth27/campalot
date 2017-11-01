71e617fcd2ab4b7c3244dc16b89e40c561e2ce32
smh28 <sandrahuldudottir@gmail.com>
2017-11-01 12:52:40 +0000

    Bætti við 2 queries í campinfoRepository (insert into... og delete from...)

--- src/main/java/is/hi/repository/campinfoRepository.java
+++ src/main/java/is/hi/repository/campinfoRepository.java
@@ -2,8 +2,10 @@ package is.hi.repository;
 
 import is.hi.model.Campinfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface campinfoRepository extends JpaRepository<Campinfo, Long>{
@@ -13,4 +15,18 @@ public interface campinfoRepository extends JpaRepository<Campinfo, Long>{
      */
     @Query(value = "SELECT n FROM Campinfo n")
     List<Campinfo> getAll();
+
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into campsitebigdata (campname, campaddress, campzip, campemail, campphone, campwebsite, campseason, maincategory, category, region, description, xval, yval) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13)"
+            , nativeQuery = true)
+    void addCamp(String name, String address, String zip, String email, String phone, String website, String season, String mainCategory, String campCategory, String campRegion, String descript, int xVal, int yVal);
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from campsitebigdata where campname=?1"
+            , nativeQuery = true)
+    void deleteCamp(String name);
+
 }
